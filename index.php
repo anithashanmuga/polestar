e306f53d7849aea5e766df7d9891cdaabd7dfc25
anithashanmuga <anitha13mar@gmail.com>
2022-09-02 00:42:30 +0530

    contact page

--- index.php
+++ index.php
@@ -282,9 +282,8 @@
       </div>
     </section><!-- End Counts Section -->
 
-
       <!-- ======= Services Section ======= -->
-      <section id="services" class="services">
+    <section id="services" class="services">
         <div class="container">
 
           <div class="section-title">
@@ -344,7 +343,6 @@
         </div>
     </section><!-- End Services Section -->
  
-
       <!-- The Modal -->
       <div id="myModal" class="modal" style="margin-top:20px;">
         <!-- Modal content -->
@@ -422,12 +420,7 @@
               <!-- <span class="recommended-badge">Recommended</span> -->
               <h3>Open desk</h3>
               <h4><sup>Rs</sup>7000<span> / month</span></h4>
-              <ul>
-                <!-- <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li> -->
-                <!-- <li class="na">Massa ultricies mi</li> -->
-              </ul>
+             
               <div class="btn-wrap">
                 <!-- <a href=""  class="btn-buy">Buy Now</a> -->
                 <button class="btn-buy" id="myBtn">Buy Now</button>
@@ -439,11 +432,7 @@
             <div class="box">
               <h3>Private cabins</h3>
               <h4><sup>Rs</sup>10000<span> / month</span></h4>
-              <ul>
-                <!-- <li>per desk</li> -->
-                <!-- <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li> -->
-              </ul>
+              
               <div class="btn-wrap">
                 <button class="btn-buy" id="myBtn">Buy Now</button>
               </div>
@@ -455,73 +444,9 @@
       </div>
     </section><!-- End Pricing Section -->
 
-     <!-- ======= Frequently Asked Questions Section ======= -->
-     <!-- <section id="faq" class="faq">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Frequently Asked Questions</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="faq-list">
-          <ul>
-            <li >
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Non consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
-                <p>
-                  Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-                </p>
-              </div>
-            </li>
-
-            <li >
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">Feugiat scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-                </p>
-              </div>
-            </li>
-
-            <li >
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-3" class="collapsed">Dolor sit amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-3" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-                </p>
-              </div>
-            </li>
-
-            <li >
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-4" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-4" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in.
-                </p>
-              </div>
-            </li>
-
-            <li >
-              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-5" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-              <div id="faq-list-5" class="collapse" data-bs-parent=".faq-list">
-                <p>
-                  Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque.
-                </p>
-              </div>
-            </li>
-
-          </ul>
-        </div>
-
-      </div>
-    </section> -->
-    <!-- End Frequently Asked Questions Section -->
-
      <!-- ======= Contact Section ======= -->
      <section id="contact" class="contact">
       <div class="container">
-
         <div class="section-title">
           <h2>Still not convinced?</h2>
           <p>Talk to us and we will hear about your
@@ -569,7 +494,7 @@
           </div>
 
           <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <form action="" method="post" role="form" class="php-email-form">
               <div class="row">
                 <div class="form-group col-md-6">
                   <label for="name">Your Name</label>
@@ -581,12 +506,12 @@
                 </div>
               </div>
               <div class="form-group">
-                <label for="name">Subject</label>
-                <input type="text" class="form-control" name="subject" id="subject" required>
+                <label for="name">Phone no</label>
+                <input type="number" class="form-control" name="phone" id="phone" required>
               </div>
               <div class="form-group">
                 <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="7" required></textarea>
+                <textarea class="form-control" name="message" rows="6" required></textarea>
               </div>
               <div class="my-3">
                 <div class="loading">Loading</div>
@@ -601,9 +526,6 @@
 
       </div>
     </section><!-- End Contact Section -->
-
-
-    
 
 </main>
 <?php include'includes/footer.php'?>
